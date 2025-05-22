9cfa363f4221646186618cf0271fcd4b26dddc30
Frederic Hodges <frederichodges@gmail.com>
2025-05-21 22:21:05 -0500

    Revert "Fix Tailwind native addon loading error"
    This reverts commit 904e34044849f22ce1bea5093783d4d15770e13a.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
@@ -11,6 +12,7 @@ export default defineConfig({
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
+        tailwindcss(),
         vue({
             template: {
                 transformAssetUrls: {
